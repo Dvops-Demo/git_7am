0af78fae59fefe6940646ebad47828f3eb1fe8de
Dvops-Demo <ashokthota90@gmail.com>
2023-08-02 00:28:50 +0530

    df2hiveTbl

---
+++ DF2HiveTbl.py
@@ -0,0 +1,16 @@
+from pyspark.sql import SparkSession
+
+from os.path import abspath
+
+warehouse_loc=abs("spark-warehouse")
+
+ssn=SparkSession.builder.master("local[*]")\
+    .appName("spark-hive-integration")\
+    .config("spark.sql.warehouse.dir",warehouse_loc) \
+    .enableHiveSupport() \
+    .getOrCreate()
+
+dfhyd=ssn.sql("select * from db.emp where loc='hyd'")
+dfhyd.write.mode("overwrite/append").saveAsTable("db.hydemp")
+
+
