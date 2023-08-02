e2a0dffd8dc783fe0360ee6148158de153a43049
Dvops-Demo <ashokthota90@gmail.com>
2023-08-03 01:24:05 +0530

    sqoop import to hive table

--- scripts/sqpImpHive.sh
+++ scripts/sqpImpHive.sh
@@ -1,5 +1,16 @@
+#export jdbc="jdbc:mysql://localhost:3306/hiveimport" this was configured in edge node
 export conn=$jdbc
+
 rdbTbl=$1
 
-sqoop import --connect conn --username=root --password=root --table rdbTbl --fields-terminated-by '|' 
---lines-terminated-by '\n' --hive-import --create-hive-table --hive-table empnew --outdir sqoop_import_project;
+echo "mysql table name is "${rdbTbl}""
+
+
+sqoop import --connect \
+"${conn}" \
+--username root --password root \
+--table "${rdbTbl}" \
+--fields-terminated-by '|' \
+--lines-terminated-by '\n' \
+--hive-import --hive-overwrite --hive-table newcars \
+--m 1
