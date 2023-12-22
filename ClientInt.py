0c0bbd08dcc81b90792e804805dfb86becd0b00f
Dvops-Demo <ashokthota90@gmail.com>
2023-12-22 21:27:48 +0530

    commiting

---
+++ ClientInt.py
@@ -0,0 +1,29 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+class ClientInt:
+
+    def readparq(self):
+        sn = SparkSession.builder.appName("lkdsmc").getOrCreate()
+        df=sn.read.parquet("s3://freelight/trash/dump/sample.parquet")
+        df.registerTempTable("emp")
+
+    def trns(self,df,sn):
+        trns_df=sn.sql("sleect * from emp where loc='hyd'")
+
+    def jsontos3(self,df):
+        df.write.json("s3://freelight/trash/jsonfilepath")
+
+        #just a commit
+
+if __name__ == '__main__':
+    ob=ClientInt()
+    ob.readparq()
+    ob.trns()
+    ob.jsontos3()
+
+
+
+
+
+
